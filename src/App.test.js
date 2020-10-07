64103f1cec3500250f6e0aa1e793f3d410d60ea7
Avinash Singh <chandrika14oct@gmail.com>
2020-10-06 17:50:24 -0700

    writing the first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDom from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders app without crashing', () => {
+  const element = document.createElement('div');
+  ReactDom.render(<App />, element);
+  expect(element.innerHTML).toContain('Hi there!');
+  ReactDom.unmountComponentAtNode(element);
 });
